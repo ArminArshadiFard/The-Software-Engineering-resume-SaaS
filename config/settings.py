4f8f53fc1cd3e46ab95a869eb0671e7aa37521a9
armin <armin1381second@gmail.com>
2026-05-29 15:47:28 +0330

    lets get started

--- config/settings.py
+++ config/settings.py
@@ -60,8 +60,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'resume_db',
+        'USER': 'resume_username',
+        'PASSWORD': 'resume_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
